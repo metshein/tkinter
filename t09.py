1a4ced0866710999db2c1e8a4199eb701cc43ded
metshein <metshein@gmail.com>
2025-02-12 11:27:54 +0200

    kõik 1-11 valmis

--- t09.py
+++ t09.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from tkinter import messagebox
 
 aken = tk.Tk()
 aken.title("Pitsapood")
+aken.geometry("300x400")
 
 def show_selection():
     # print("Pitsa suurus:", selected_size.get())
@@ -15,15 +17,18 @@ def show_selection():
     trans = hinnad[nr]
     kokku = suurus+lisad+trans
     print(kokku)
+    messagebox.showinfo("Pitsa hind", f"Tasumisele kuulub {kokku:.2f}€")
     
+tk.Label(aken, text=f"Kasutaja ID:", font="Arial 14").pack()
+tk.Entry(aken).pack()
 
 # Suuruse hind
 selected_size = tk.IntVar(value=5)
-
+tk.Label(aken, text=f"Vali pitsa suurus:", font="Arial 14").pack()
 # Loome raadionupud
-radio_red = tk.Radiobutton(aken, text="Väike (5€)", variable=selected_size, value=5)
-radio_green = tk.Radiobutton(aken, text="Suur (8€)", variable=selected_size, value=8)
-radio_blue = tk.Radiobutton(aken, text="Pere (12€)", variable=selected_size, value=12)
+radio_red = tk.Radiobutton(aken, text="Väike (5€)", font="Arial 12", variable=selected_size, value=5)
+radio_green = tk.Radiobutton(aken, text="Suur (8€)", font="Arial 12", variable=selected_size, value=8)
+radio_blue = tk.Radiobutton(aken, text="Pere (12€)", font="Arial 12", variable=selected_size, value=12)
 radio_red.pack()
 radio_green.pack()
 radio_blue.pack()
@@ -31,7 +36,7 @@ radio_blue.pack()
 var1 = tk.IntVar(value=0)
 var2 = tk.DoubleVar(value=0)
 var3 = tk.IntVar(value=0)
-
+tk.Label(aken, text=f"Vali lisandid:", font="Arial 14").pack()
 # Loome märkeruudud
 checkbox1 = tk.Checkbutton(aken, text="Juust (+1€)", variable=var1, onvalue=1, offvalue=0)
 checkbox2 = tk.Checkbutton(aken, text="Pepperoni (+1.5€)", variable=var2, onvalue=1.5, offvalue=0)
@@ -41,6 +46,7 @@ checkbox2.pack()
 checkbox3.pack()
 
 # Dropdown valikud
+tk.Label(aken, text=f"Vali transport:", font="Arial 14").pack()
 valikud = ["Kuller (+3€)", "Tulen ise järgi (0€)"]
 selected_option = tk.StringVar()
 selected_option.set("Kuller (+3€)")
@@ -50,6 +56,6 @@ dropdown = tk.OptionMenu(aken, selected_option, *valikud)
 dropdown.pack()
 
 btn_confirm = tk.Button(aken, text="Kinnita valik", command=show_selection)
-btn_confirm.pack()
+btn_confirm.pack(pady=20)
 
 aken.mainloop()
